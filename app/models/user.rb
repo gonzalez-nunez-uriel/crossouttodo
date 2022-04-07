e9b418ca04b799b4cfe8e750b32d8d4aac80e254
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-04-07 00:31:33 -0500

    validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    # Do I have to provide attribute getters? Things work just fine in the rails console.
+    validates :name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
 end
