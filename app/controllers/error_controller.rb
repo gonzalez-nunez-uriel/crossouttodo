06755747993261ff13ea3de8279dae500e76e220
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-04-08 15:23:32 -0500

    changed error logic to handle better an empty password field

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -1,6 +1,11 @@
 class ErrorController < ApplicationController
     def registration_validation_error
-        render template: 'error/registration/validation-error'
+        if cookies[:ValidationError] == nil
+            render 'error/registration/generic-validation-error'
+        else
+            @errors = cookies[:ValidationError]
+            render template: 'error/registration/validation-error'
+        end
     end
 
     def registration_password_mismatch
