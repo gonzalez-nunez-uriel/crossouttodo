af853a441ffb58c5f61870057dd2b05b118e4264
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-04-09 23:18:41 -0500

    Implemented quick log in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if session[:session_string] != nil
+      @session_exists = true
+    else
+      @session_exists = false
+    end
   end
 end
