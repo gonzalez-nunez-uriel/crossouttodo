749e4a3a41fc72ef4349a1dc9434acaddb4f6d3e
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-04-10 15:26:52 -0500

    More appealing quick log in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,26 @@
 class HomeController < ApplicationController
   def index
+
     if session[:session_string] != nil
-      @session_exists = true
+
+      browser_session =  Session.find_by( session_string: session[ :session_string ] )
+
+      if browser_session != nil
+
+        @user = User.find( browser_session.user_id )
+
+        if @user != nil
+          @user_exists = true
+        else
+          @user_exists = false
+        end
+
+      else
+        @user_exists = false
+      end
+
     else
-      @session_exists = false
+      @user_exists = false
     end
   end
 end
