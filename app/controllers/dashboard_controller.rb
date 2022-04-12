71744dd431a00d3b4cc51eec667133b98e10e6af
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-04-12 08:12:32 -0500

    Refactored

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -33,39 +33,26 @@ class DashboardController < ApplicationController
 
   def insert
 
-    if session[:session_string] != nil
+    error_redirect_url, user = DashboardHelper.get_user session[ :session_string ]
 
-      browser_session = Session.find_by( session_string: session[ :session_string ] )
+    if error_redirect_url == nil
 
-      if browser_session != nil
+      begin
+        
+        new_task = DashboardHelper.generate_new_task user.id, params[:name], params[:summary], params[:description], params[:deadline]
+        new_task.save!
 
-        user = User.find( browser_session.user_id )
+        redirect_to '/dashboard'
 
-        if user != nil
+      rescue ActiveRecord::RecordInvalid => e
 
-          begin
-            new_task = DashboardHelper.generate_new_task user.id, params[:name], params[:summary], params[:description], params[:deadline]
-            new_task.save!
-  
-            redirect_to '/dashboard'
-
-          rescue ActiveRecord::RecordInvalid => e
-
-            cookies[:ValidationError] = { value: e.to_s, expires: 3.minutes.from_now }
-            redirect_to '/error/bad-task/validation-error'
-            
-          end
-
-        else
-          redirect_to 'error/user-does-not-exists'
-        end
-
-      else
-        redirect_to 'error/session-not-found'
+        cookies[:ValidationError] = { value: e.to_s, expires: 3.minutes.from_now }
+        redirect_to '/error/bad-task/validation-error'
+        
       end
 
     else
-      redirect_to 'error/not-logged-in'
+      redirect_to error_redirect_url
     end
 
   end
